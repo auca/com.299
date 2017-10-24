74ee48c302663a36742051097913ef146107c79f
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2017-10-24 23:27:44 +0600

    Add Practice 3 sources

---
+++ Project 1/Practice 3/BallController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+        public float InputForceScale =
+                10.0f;
+        public float InitialAngle =
+                20.0f;
+
+        public AudioClip WallSound;
+        public AudioClip PaddleSound;
+
+        private AudioSource audioSource;
+        private Rigidbody rigidBody;
+
+        void Start () {
+                audioSource =
+                        GetComponent<AudioSource> ();
+
+                rigidBody =
+                        GetComponent<Rigidbody> ();
+
+                Vector3 force =
+                        Quaternion.Euler (0.0f, InitialAngle, 0.0f) * Vector3.forward *
+                            InputForceScale;
+
+                rigidBody.AddForce (force);
+        }
+
+        void OnCollisionEnter(Collision collision) {
+                GameObject gameObject =
+                        collision.gameObject;
+
+                if (gameObject.CompareTag ("Wall")) {
+                        audioSource.PlayOneShot (WallSound);
+                } else if (gameObject.CompareTag ("Paddle")) {
+                        audioSource.PlayOneShot (PaddleSound);
+                }
+        }
+
+}
+
---
+++ Project 1/Practice 3/PaddleController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+
+        public float InputForceScale =
+                300.0f;
+        public float ForceAppliedToBallScale =
+                4.0f;
+
+        private Rigidbody rigidBody;
+
+        void Start () {
+                rigidBody =
+                        GetComponent<Rigidbody> ();
+        }
+
+        void FixedUpdate () {
+                float horizontalAxis =
+                        Input.GetAxis ("Horizontal");
+
+                Vector3 force =
+                        new Vector3 (horizontalAxis, 0.0f, 0.0f) *
+                                InputForceScale;
+
+                rigidBody.AddForce (force);
+        }
+
+        void OnCollisionEnter(Collision collision) {
+                GameObject gameObject =
+                        collision.gameObject;
+
+                if (gameObject.CompareTag ("Ball")) {
+                        GameObject ball =
+                                gameObject;
+
+                        float shift =
+                                ball.transform.position.x -
+                                        transform.position.x;
+
+                        Vector3 force =
+                                new Vector3 (shift, 0.0f, 0.0f) *
+                                        ForceAppliedToBallScale;
+
+                        ball.GetComponent<Rigidbody> ().AddForce (force);
+                }
+        }
+}
+
